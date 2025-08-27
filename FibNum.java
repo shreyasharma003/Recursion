61beeba9dc8a880f578bcbb8525a6f6006dad967
shreyasharma003 <shreyusharma11@gmail.com>
2025-08-27 23:22:06 +0530

    27 August

---
+++ FibNum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FibNum {
+
+    
+    static int fib(int n){
+        if(n<2){
+            return n;
+        }
+
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fib(n));
+
+        sc.close();
+    }
+
+}
+
