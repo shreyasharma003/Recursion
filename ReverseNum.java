f11bd4a4c4fdb5f57a297a57963fb14908121df4
shreyasharma003 <shreyusharma11@gmail.com>
2025-08-28 23:04:00 +0530

    28 August

---
+++ ReverseNum.java
@@ -0,0 +1,18 @@
+public class ReverseNum{
+    public static void main(String[] args) {
+        rev(9653);
+        System.out.println(sum);
+    }
+
+    static int sum = 0;
+    static void rev(int n){
+        if(n==0){
+            return;
+        }
+        int rem = n%10;
+        sum = sum*10 + rem;
+
+        rev(n/10);
+
+    }
+}
